a21bafb7ae5ea139d29125e01f4ccf1632e7b5dd
Joe_Nasa <joaovictorextremo156@gmail.com>
2023-08-07 15:15:14 -0300

    menu

--- my-joe/src/app/page.tsx
+++ my-joe/src/app/page.tsx
@@ -1,16 +1,35 @@
 import Link from "next/link"
+import './css.css'
 
 export default function Home() {
   return (
     <main>
-      <img className="w-full h-full flex"
-      src="https://i.pinimg.com/originals/7b/e5/a1/7be5a1cb85ca4d0483c1ff17a487d50a.gif"
+      <picture>
+      <img 
+      className="w-full flex"
+      src="https://i.pinimg.com/originals/8a/63/3d/8a633dbb14009411650a5a914d73c950.gif"
       />
-      <div className=" fixed top-0 left-[39vw] flex">
-        <Link href='/Catpage' className=" p-[2vw] pt-[1.5vw] pr-[3vw] text-[1.5vw]">𝓒𝓪𝓽𝓼</Link>
+      <img 
+      className="w-full flex"
+      src="https://i.pinimg.com/originals/1e/b5/4e/1eb54ebc7da10c609cb0ba9268facef9.gif"
+      />
+      </picture>
+      <div className=" fixed top-[1vw] left-[1vw]">
+      <div className="menu">
+        <button>Sites</button>
+        <div className="menu-content">
+        <li><Link href='/Moonpage'>𝓢𝓽𝓪𝓻-𝓝𝓲𝓰𝓱𝓽t</Link></li>
+        <li><Link href='/Catpage'>𝓒𝓪𝓽𝓼</Link></li>
+      </div>
+      </div>
+      </div>
+      <div className=" fixed top-0 left-[47vw] flex">
         <p className=" text-[3vw]">𝔍𝔬𝔢</p>
-        <Link href='/Moonpage' className=" p-[2vw] pt-[1.5vw] pl-[3vw] text-[1.5vw]">𝓢𝓽𝓪𝓻-𝓝𝓲𝓰𝓱𝓽t</Link>
         </div>
+        <div className="absolute top-[20vw] left-[35vw]">
+          <p className="text-[10vw]">ɪɴɪᴄɪᴏ</p>
+        </div>
+      
       </main>
   )
 }
